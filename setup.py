6d889941ab3869f701a93a0297387ca45a63f627
Francois Blackburn <fblackburn@wazo.community>
2018-03-08 11:47:43 -0500

    add base nestbox-confd-client

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='nestbox_confd_client',
+    version='0.1',
+    description='a simple client library for the nestbox confd HTTP interface',
+    author='Wazo Authors',
+    author_email='dev@wazo.io',
+    url='http://wazo.io',
+    packages=find_packages(),
+    entry_points={
+        'nestbox_confd_client.commands': [
+        ],
+    }
+)
