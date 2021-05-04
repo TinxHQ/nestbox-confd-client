698bc3fd1745faad4db713a356cccca7d747a46c
Francois Blackburn <fblackburn@wazo.community>
2021-05-04 11:20:00 -0400

    fix typo from blackify

--- nestbox_confd_client/commands/engines.py
+++ nestbox_confd_client/commands/engines.py
@@ -16,7 +16,7 @@ class EnginesCommand(ConfdCommand):
         self.raise_from_response(r)
 
     def check_accounts_hash(self, instance_uuid, hash_):
-        url = f'{self.base_url}/instances/{instance_uuid}/synchronize'
+        url = f'{self.base_url}/instances/{instance_uuid}/accounts/synchronize'
         params = {'hash': hash_}
         r = self.session.head(url, params=params, headers=self._ro_headers)
         if r.status_code in (204, 409):
@@ -24,7 +24,7 @@ class EnginesCommand(ConfdCommand):
         self.raise_from_response(r)
 
     def synchronize_accounts(self, instance_uuid, accounts):
-        url = f'{self.base_url}/instances/{instance_uuid}/synchronize'
+        url = f'{self.base_url}/instances/{instance_uuid}/accounts/synchronize'
         body = {'accounts': accounts}
         r = self.session.post(url, json=body, headers=self._rw_headers)
         self.raise_from_response(r)
