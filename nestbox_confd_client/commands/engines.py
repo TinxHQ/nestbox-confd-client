f8464537b411118b537434e2499b001c93eadc1d
Francois Blackburn <fblackburn@wazo.community>
2023-02-01 08:20:19 -0500

    fix new black rules

--- nestbox_confd_client/commands/engines.py
+++ nestbox_confd_client/commands/engines.py
@@ -1,11 +1,10 @@
-# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from nestbox_confd_client.command import ConfdCommand
 
 
 class EnginesCommand(ConfdCommand):
-
     resource = 'engines'
 
     def create_account_event(self, instance_uuid, event):
