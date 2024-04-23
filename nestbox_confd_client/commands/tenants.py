83e5926baa7b7289ff4a814e930d0464debc9279
Sebastien Duthil <sduthil@wazo.community>
2024-04-23 14:09:58 +0200

    commands: add tenant_uuid parameter everywhere
    Why:
    
    * Don't need to change the global tenant of the client

--- nestbox_confd_client/commands/tenants.py
+++ nestbox_confd_client/commands/tenants.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from nestbox_confd_client.command import ConfdCommand
@@ -7,8 +7,8 @@ from nestbox_confd_client.command import ConfdCommand
 class TenantsCommand(ConfdCommand):
     resource = 'tenants'
 
-    def get(self, tenant_uuid):
-        headers = self._get_headers()
+    def get(self, tenant_uuid, scoping_tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=scoping_tenant_uuid)
         url = f'{self.base_url}/{tenant_uuid}'
         r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
